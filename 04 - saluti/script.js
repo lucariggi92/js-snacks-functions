6a71c71663f18f0a6cb166b170fb08fa1d4443e8
lucariggi92 <riggiluca92@gmail.com>
2025-11-04 17:15:34 +0100

    feat: saluti completed

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,18 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
+const name = 'Luca';
 
 
 // Dichiara la funzione qui.
+function saluta (soggetto){
+    return newSaluto = "Ciao" + " " + soggetto
+}
+
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(saluta(name));
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
