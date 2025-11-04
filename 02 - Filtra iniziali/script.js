b160cbef1476fe7d6eb2ff9a60819fc4dbdf48e8
lucariggi92 <riggiluca92@gmail.com>
2025-11-04 16:34:18 +0100

    feat: filtra completed

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,6 +4,19 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function NameWithFirstletter(name, firstletter){
+    const newArray = [];
+    for(let i=0; i<name.length; i++){
+       const curName = name[i];
+        const first  = curName.charAt(0)
+    //verifico che la pria lettera sia uguale a quella desisderata "A"
+        if(first === firstletter){
+            newArray.push(curName)
+        }
+    }
+    //chiudo con return, se non verifica procedo con il for
+       return newArray;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
@@ -11,3 +24,4 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+console.log(NameWithFirstletter(names, "A"));
