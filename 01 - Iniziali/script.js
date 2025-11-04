f1d8af08aa5852905a5920d19ebe5f0c09d6e70a
lucariggi92 <riggiluca92@gmail.com>
2025-11-04 16:13:50 +0100

    feat: iniziali completed

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -5,9 +5,25 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function firstLetter(nomi) {
+    const newArray = [];
+    //scorro ogni singolo nome 
+    for (let i = 0; i < nomi.length; i++) {
+        //trovo il singolo nome
+        let curNome = nomi[i]
+        //estraggo la prima lettera
+        let first = curNome.charAt(0)
+        //la pusho nel nuovo array
+        newArray.push(first)
+    }
+
+    //chiudo con return
+    return newArray
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(firstLetter(names))
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
